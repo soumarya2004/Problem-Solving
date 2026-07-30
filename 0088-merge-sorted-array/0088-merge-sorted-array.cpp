e08c122830d36bbae0813422ea0cfe59f8900bd5
soumarya2004 <soumaryamandal12@gmail.com>
2026-07-30 19:15:09 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,20 +6,14 @@ public:
         int k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-                k--;
+                nums1[k--]=nums1[i--];
             }
             else{
-                nums1[k]=nums2[j];
-                j--;
-                k--;
+                nums1[k--]=nums2[j--];
             }
         }
         while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
+            nums1[k--]=nums2[j--];
         }
     }
 };
