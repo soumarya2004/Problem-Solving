6e138e7030cdda9fae9b9d662d7dde9642955702
soumarya2004 <soumaryamandal12@gmail.com>
2026-08-03 19:13:21 +0530

    Time: 4 ms (3.99%), Space: 8.4 MB (39.29%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int fun(int n){
+    int getSum(int n){
         int sum=0;
         while(n>0){
-            int d=n%10;
-            n=n/10;
-            sum+=d*d;
+            int digit=n%10;
+            sum+=digit*digit;
+            n/=10;
         }
         return sum;
     }
     bool isHappy(int n) {
-        int slow=n, fast=n;
-        while(fast!=1){
-            slow=fun(slow);
-            fast=fun(fun(fast));
-            if(slow==fast && slow!=1){
-                return false;
-            }
+        unordered_set<int> st;
+        while(n!=1 && !st.count(n)){
+            st.insert(n);
+            n=getSum(n);
         }
-        return true;
+        return n==1;
     }
 };
